9ba120fe60e2442e69965a0040f81a37637ba0d6
AlexRDN8 <alexrodin8@bk.ru>
2022-12-18 21:22:34 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,]array = new int[3,4]
+{
+    {1,4,7,2},
+    {5,9,2,3},
+    {8,4,2,4}
+};
+
+GetMeanOfColumns (array);
+
+void GetMeanOfColumns (int[,] array)
+{    
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i,j];
+        }
+        double result = sum / array.GetLength(0);
+        Console.WriteLine(result);
+    }
+    
+}
